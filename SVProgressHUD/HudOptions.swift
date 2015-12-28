e24b7eb6073e7fd3fd86448a0cbedb7c81da7b2f
PeeJWeeJ <pj.fechner@gmail.com>
2015-12-27 21:17:07 -0600

    Work on a nib for the view

---
+++ SVProgressHUD/HudOptions.swift
@@ -0,0 +1,15 @@
+//
+//  HudOptions.swift
+//  SVProgressHUD
+//
+//  Created by Paul Fechner on 12/27/15.
+//  Copyright © 2015 EmbeddedSources. All rights reserved.
+//
+
+struct HudOptions {
+
+	var hudStyle: ProgressHUDStyle = .Light
+	var hudMaskType: ProgressHUDMaskType = .None
+	var hudAnimationType:ProgressHUDAnimationType = .Native
+	
+}
